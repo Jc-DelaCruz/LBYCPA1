d2b683001854ee9253c09a8065d2739e2e264e6d
Jc-DelaCruz <john_carlo_delacruz@dlsu.edu.ph>
2022-06-20 23:25:15 +0800

    update

--- Project/login.py
+++ Project/login.py
@@ -8,10 +8,75 @@ def pause():
 
 def gpa_calculator():
     print("Loading... GPA Calculator Function ---")
-    pass
+    # pass
+
+def flowchartExit():
+    print()
+    print("Do you want to view other Flowcharts?")
+    print("1 - Flowcharts Restart")
+    print("2 - Main Menu")
+    print("2 - Exit")
+    while True:
+        print()
+        ch = input("Choose a Term: ")
+        if ch in ['1','2','3']:
+            break
+    if ch == '1':
+        pause()
+        flowchart()
+    elif ch == '2':
+        pause()
+        mainMenu()
+    elif ch == '3':
+        print("Thank you for using the program!")
+        pause()
 
 def flowchart():
-    print("Loading... Flowchart Function ---")
+    clear()
+    print("=====================")
+    print("Flowchart")
+    print("=====================")
+    print("1 - 1st Term")
+    print("2 - 2nd Term")
+    print("3 - 3rd Term")
+    print("4 - Exit")
+    while True:
+        print()
+        term = input("Choose a Term: ")
+        if term in ['1','2','3','4']:
+            break
+        
+    if term == '1':
+        with open('term1.txt') as table1:
+            term1 = table1.read()
+            print("\n",term1)
+        flowchartExit()
+
+
+    elif term == '2':
+        with open('term2.txt') as table2:
+            term2 = table2.read()
+            print("\n",term2)
+        flowchartExit()
+
+
+    elif term == '3':
+        with open('term3.txt') as table3:
+            term3 = table3.read()
+            print("\n",term3)
+        flowchartExit()
+
+    elif term == '4':
+        print("Thank you for using the Program!")
+        pause()
+        return 0
+
+    else:
+        print("Invalid Input... Try Again..")
+        pause()
+        flowchart()
+    
+    
     # pass
 
 def help_desk():
@@ -29,10 +94,11 @@ def mainMenu():
     print("1 - GPA Calculator")
     print("2 - Flowcharts")
     print("3 - Help Desk / FAQs")
+    print("4 - Exit Program")
     while True:
         print()
         userCh = input("Choose an Option: ")
-        if userCh in ['1', '2', '3']:
+        if userCh in ['1', '2', '3','4']:
             break
     if userCh == '1':
         gpa_calculator()
@@ -40,6 +106,10 @@ def mainMenu():
         flowchart()
     elif userCh == '3':
         help_desk()
+    elif userCh == '4':
+        print("Thank you for using the Program!")
+        pause()
+        return 0
     else:
         print("Invalid Input Try again...")
         pause()
@@ -102,16 +172,21 @@ def main():
     print()
     print("1 - Register")
     print("2 - Login")
+    print("3 - Exit")
     print()
     while True:
         print()
         userCh = input("Choose an Option: ")
-        if userCh in ['1', '2']:
+        if userCh in ['1', '2','3']:
             break
     if userCh == '1':
         register()
     elif userCh == '2':
         login()
+    elif userCh == '3':
+        print("Thank you for using the Program!")
+        pause()
+        return 0
     else:
         print("Invalid input Try again...")
         pause()
---
+++ Project/testfile.py
@@ -0,0 +1,4 @@
+with open('term1.txt') as table1:
+    term1 = table1.read()
+    print(term1)
+
